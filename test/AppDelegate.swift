676bb289ece6e397c9d11c6251da66bcca82a4e5
olderor <olderor@Mac-olderor.local>
2017-01-15 16:50:16 +0200

    Initial Commit

---
+++ test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by olderor on 15.01.17.
+//  Copyright © 2017 olderor. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
